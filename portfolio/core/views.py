a7efb46b2f19a65da6b74b5f14adb1c52fe0f5f2
vinhlam1994 <khaiductam@gmail.com>
2023-10-28 12:05:19 +0700

    Update

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -1,3 +1,16 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
 
-# Create your views here.
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    # override get context date method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) # first, call super get context data
+        context['about'] = About.objects.first()
+        context['services'] = Service.objects.all()
+        context['work'] = RecentWork.objects.all()
+        return context
