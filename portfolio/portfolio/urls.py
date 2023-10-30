dcdd593aebb4690199230f2859d4359262baa55f
vinhlam1994 <khaiductam@gmail.com>
2023-10-30 17:30:07 +0700

    Create user and add Data

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')), # include our custom app urls here
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
