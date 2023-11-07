0abe9f7770b06fd45d867f9f66ee071c718029c1
vinhlam1994 <khaiductam@gmail.com>
2023-11-07 22:33:22 +0700

    Update 7-11-2023

--- w3s/kendo_club/kendo_club/settings.py
+++ w3s/kendo_club/kendo_club/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'kendo_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kendo_club',
+        'USER': 'postgres',
+        'PASSWORD': 'Doov@1994',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- w3s/kendo_club/members/models.py
+++ w3s/kendo_club/members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    membersAge = models.IntegerField(default=0)
+
+class Level(models.Model):
+    firstname = models.ForeignKey(Member, on_delete=models.CASCADE)
+    danLevel = models.IntegerField(default=0)
